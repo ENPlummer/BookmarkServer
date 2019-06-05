ea0d38ba0ffe7bbedd808b91c279a8fe6222db97
Ebony N. Plummer <ebony.plummer@gmail.com>
2019-06-04 21:28:17 -0400

    Changed the port number of the server.

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -148,6 +148,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plainl charset=utf-8')
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    server_address = ('', 80)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
